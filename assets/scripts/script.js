c2b5abefe8e90af2d02df0e264849471d6034b65
glebQuanta <gbum@quantatech.net>
2024-07-26 12:50:36 +0300

    sample

---
+++ assets/scripts/script.js
@@ -0,0 +1,519 @@
+
+ const initialData = [
+
+    {
+        "name": "Jack",
+        "entryPoint": 123.45,
+        "PnL": "+342,60$",
+        "ROI": "+101,1%",
+        "Take": 10.00,
+        "NumAttempts": 3,
+        "Margin": "20$",
+        "Funding": "100$",
+        "Size": 1500,
+        "Time": "44 hours",
+        "allTime": "1 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Sasha",
+        "entryPoint": 127.89,
+        "PnL": "+442,60$",
+        "ROI": "+102,1%",
+        "Take": 5.50,
+        "NumAttempts": 1,
+        "Margin": "35$",
+        "Funding": "100$",
+        "Size": 2000,
+        "Time": "24 hours",
+        "allTime": "2 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Anton",
+        "entryPoint": 130.56,
+        "PnL": "-42,60$",
+        "ROI": "-10,1%",
+        "Take": 8.25,
+        "NumAttempts": 2,
+        "Margin": "18$",
+        "Funding": "100$",
+        "Size": 1750,
+        "Time": "14 hours",
+        "allTime": "3 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Oleg",
+        "entryPoint": 60.56,
+        "PnL": "-422,60$",
+        "ROI": "-120,1%",
+        "Take": 82.25,
+        "NumAttempts": 22,
+        "Margin": "128$",
+        "Funding": "1200$",
+        "Size": 2750,
+        "Time": "124 hours",
+        "allTime": "4 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Masha",
+        "entryPoint": 360.56,
+        "PnL": "+4232,60$",
+        "ROI": "+120,1%",
+        "Take": 832.25,
+        "NumAttempts": 232,
+        "Margin": "1328$",
+        "Funding": "3200$",
+        "Size": 3750,
+        "Time": "324 hours",
+        "allTime": "1.5 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Evgen",
+        "entryPoint": 560.56,
+        "PnL": "-5232,60$",
+        "ROI": "-520,1%",
+        "Take": 532.25,
+        "NumAttempts": 532,
+        "Margin": "5328$",
+        "Funding": "5200$",
+        "Size": 5750,
+        "Time": "524 hours",
+        "allTime": "2.2 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Gleb",
+        "entryPoint": 1060.56,
+        "PnL": "+10232,60$",
+        "ROI": "+20020,1%",
+        "Take": 1032.25,
+        "NumAttempts": 1032,
+        "Margin": "1028$",
+        "Funding": "10200$",
+        "Size": 10750,
+        "Time": "24 hours",
+        "allTime": "3.1 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Anastasiya",
+        "entryPoint": 2060.56,
+        "PnL": "-22232,60$",
+        "ROI": "-22020,1%",
+        "Take": -1032.25,
+        "NumAttempts": 1032,
+        "Margin": "1028$",
+        "Funding": "10200$",
+        "Size": 20750,
+        "Time": "1 hours",
+        "allTime": "5 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Dinara",
+        "entryPoint": 160.56,
+        "PnL": "+12,60$",
+        "ROI": "+123,1%",
+        "Take": +232.25,
+        "NumAttempts": 2032,
+        "Margin": "2028$",
+        "Funding": "220$",
+        "Size": 2750,
+        "Time": "2 hours",
+        "allTime": "1 day",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Dmitri",
+        "entryPoint": 660.56,
+        "PnL": "+62,60$",
+        "ROI": "+623,1%",
+        "Take": +632.25,
+        "NumAttempts": 6032,
+        "Margin": "6028$",
+        "Funding": "620$",
+        "Size": 6750,
+        "Time": "6 hours",
+        "allTime": "6 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Dominik",
+        "entryPoint": 760.56,
+        "PnL": "-72,60$",
+        "ROI": "-723,1%",
+        "Take": -732.25,
+        "NumAttempts": 7032,
+        "Margin": "7028$",
+        "Funding": "720$",
+        "Size": 7750,
+        "Time": "7 hours",
+        "allTime": "2 day",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Eva",
+        "entryPoint": 860.56,
+        "PnL": "+82,60$",
+        "ROI": "+873,1%",
+        "Take": +852.25,
+        "NumAttempts": 8032,
+        "Margin": "8528$",
+        "Funding": "840$",
+        "Size": 8750,
+        "Time": "8 hours",
+        "allTime": "1 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Jack2",
+        "entryPoint": 123.45,
+        "PnL": "+342,60$",
+        "ROI": "+101,1%",
+        "Take": 10.00,
+        "NumAttempts": 3,
+        "Margin": "20$",
+        "Funding": "100$",
+        "Size": 1500,
+        "Time": "44 hours",
+        "allTime": "2 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Sasha2",
+        "entryPoint": 127.89,
+        "PnL": "+442,60$",
+        "ROI": "+102,1%",
+        "Take": 5.50,
+        "NumAttempts": 1,
+        "Margin": "35$",
+        "Funding": "100$",
+        "Size": 2000,
+        "Time": "24 hours",
+        "allTime": "3 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Anton2",
+        "entryPoint": 130.56,
+        "PnL": "-42,60$",
+        "ROI": "-10,1%",
+        "Take": 8.25,
+        "NumAttempts": 2,
+        "Margin": "18$",
+        "Funding": "100$",
+        "Size": 1750,
+        "Time": "14 hours",
+        "allTime": "4.2 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Oleg2",
+        "entryPoint": 60.56,
+        "PnL": "-422,60$",
+        "ROI": "-120,1%",
+        "Take": 82.25,
+        "NumAttempts": 22,
+        "Margin": "128$",
+        "Funding": "1200$",
+        "Size": 2750,
+        "Time": "124 hours",
+        "allTime": "2.1 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Masha2",
+        "entryPoint": 360.56,
+        "PnL": "+4232,60$",
+        "ROI": "+120,1%",
+        "Take": 832.25,
+        "NumAttempts": 232,
+        "Margin": "1328$",
+        "Funding": "3200$",
+        "Size": 3750,
+        "Time": "324 hours",
+        "allTime": "1.5 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Evgen2",
+        "entryPoint": 560.56,
+        "PnL": "-5232,60$",
+        "ROI": "-520,1%",
+        "Take": 532.25,
+        "NumAttempts": 532,
+        "Margin": "5328$",
+        "Funding": "5200$",
+        "Size": 5750,
+        "Time": "524 hours",
+        "allTime": "10 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Gleb2",
+        "entryPoint": 1060.56,
+        "PnL": "+10232,60$",
+        "ROI": "+20020,1%",
+        "Take": 1032.25,
+        "NumAttempts": 1032,
+        "Margin": "1028$",
+        "Funding": "10200$",
+        "Size": 10750,
+        "Time": "24 hours",
+        "allTime": "2 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Anastasiya2",
+        "entryPoint": 2060.56,
+        "PnL": "-22232,60$",
+        "ROI": "-22020,1%",
+        "Take": -1032.25,
+        "NumAttempts": 1032,
+        "Margin": "1028$",
+        "Funding": "10200$",
+        "Size": 20750,
+        "Time": "1 hours",
+        "allTime": "3 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Dinara2",
+        "entryPoint": 160.56,
+        "PnL": "+12,60$",
+        "ROI": "+123,1%",
+        "Take": +232.25,
+        "NumAttempts": 2032,
+        "Margin": "2028$",
+        "Funding": "220$",
+        "Size": 2750,
+        "Time": "2 hours",
+        "allTime": "4 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Dmitri2",
+        "entryPoint": 660.56,
+        "PnL": "+62,60$",
+        "ROI": "+623,1%",
+        "Take": +632.25,
+        "NumAttempts": 6032,
+        "Margin": "6028$",
+        "Funding": "620$",
+        "Size": 6750,
+        "Time": "6 hours",
+        "allTime": "3.2 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Dominik2",
+        "entryPoint": 760.56,
+        "PnL": "-72,60$",
+        "ROI": "-723,1%",
+        "Take": -732.25,
+        "NumAttempts": 7032,
+        "Margin": "7028$",
+        "Funding": "720$",
+        "Size": 7750,
+        "Time": "7 hours",
+        "allTime": "1 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+    {
+        "name": "Eva2",
+        "entryPoint": 860.56,
+        "PnL": "+82,60$",
+        "ROI": "+873,1%",
+        "Take": +852.25,
+        "NumAttempts": 8032,
+        "Margin": "8528$",
+        "Funding": "840$",
+        "Size": 8750,
+        "Time": "8 hours",
+        "allTime": "1 year",
+        "allTimePnL": "+342,60$",
+        "allTimeRoi": "+101,1%",
+        "allTimePay": "5000$"
+    },
+
+];
+
+const formatCurrency = (value) => parseFloat(value.replace(/[^\d.,-]/g, '').replace(',', '.'));
+
+const preprocess = (value, key) => {
+    switch (key) {
+        case 'PnL':
+        case 'ROI':
+        case 'Margin':
+        case 'Funding':
+            return formatCurrency(value);
+        case 'Time':
+            return parseFloat(value.replace(' hours', ''));
+        default:
+            return isNaN(value) ? value : parseFloat(value);
+    }
+};
+
+const populateTable = (data) => {
+    const tableBody = document.getElementById('tableBody');
+    tableBody.innerHTML = '';
+
+    data.forEach(item => {
+        const row = document.createElement('tr');
+        row.className = item.PnL.startsWith('+') ? 'positive' : 'negative';
+        row.innerHTML = `
+            <td onclick='openPopup(${JSON.stringify(item).replace(/'/g, "\\'")})'>${item.name}</td>
+            <td>${item.entryPoint}</td>
+            <td>${item.PnL}</td>
+            <td>${item.ROI}</td>
+            <td>${item.Take}</td>
+            <td>${item.NumAttempts}</td>
+            <td>${item.Margin}</td>
+            <td>${item.Funding}</td>
+            <td>${item.Size}</td>
+            <td>${item.Time}</td>
+        `;
+        tableBody.appendChild(row);
+    });
+};
+
+const updateSortIcons = (key, direction) => {
+    const headers = document.querySelectorAll('#dataTable th');
+    headers.forEach(header => {
+        header.classList.remove('ascending', 'descending', 'default');
+        if (header.getAttribute('onclick') && header.getAttribute('onclick').includes(key)) {
+            if (direction === 'ascending') {
+                header.classList.add('ascending');
+            } else if (direction === 'descending') {
+                header.classList.add('descending');
+            } else {
+                header.classList.add('default');
+            }
+        }
+    });
+};
+
+const sortTable = (key) => {
+    let direction = 'ascending';
+
+    if (sortConfig.key === key && sortConfig.direction === 'ascending') {
+        direction = 'descending';
+    } else if (sortConfig.key === key && sortConfig.direction === 'descending') {
+        direction = 'default';
+    }
+
+    const sortedData = [...data].sort((a, b) => {
+        const aValue = preprocess(a[key], key);
+        const bValue = preprocess(b[key], key);
+
+        if (aValue > bValue) return direction === 'ascending' ? 1 : -1;
+        if (aValue < bValue) return direction === 'ascending' ? -1 : 1;
+        return 0;
+    });
+
+    data = direction === 'default' ? initialData : sortedData;
+    sortConfig = { key, direction };
+    populateTable(data);
+    updateSortIcons(key, direction); 
+};
+
+const openPopup = (item) => {
+    const popup = document.getElementById('popup');
+    const popupContent = document.getElementById('popupContent');
+    popupContent.innerHTML = `
+        <h2>${item.name}</h2>
+        <p>All Time: ${item.allTime}</p>
+        <p>All Time PnL: ${item.allTimePnL}</p>
+        <p>All Time ROI: ${item.allTimeRoi}</p>
+        <p>All Time Pay: ${item.allTimePay}</p>
+    `;
+ 
+    popup.style.display = 'flex';
+    document.body.style.overflow = 'hidden';
+};
+
+const closePopup = () => {
+
+    document.getElementById('popup').style.display = 'none';
+    document.body.style.overflow = 'auto';
+};
+
+let data = initialData;
+let sortConfig = { key: '', direction: 'default' };
+
+document.addEventListener('DOMContentLoaded', () => {
+    populateTable(data);
+});
